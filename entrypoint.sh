8934910b5e79bc51fa457f93ecd7f43a790e4759
Gegham Zakaryan <zakaryan.2004@outlook.com>
2026-02-01 00:20:07 +0400

    entrypoint: Add support for --patchonly flag
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,15 @@ sudo dnf builddep -y rpmbuild/SRPMS/kernel.src.rpm
 echo ">>> Extracting kernel source from SRPM..."
 rpm -i rpmbuild/SRPMS/kernel.src.rpm
 
-# Apply patches if /patches directory exists and has .patch files
+# If --patchonly is provided, check if there are patches to apply
+if [ "$1" == "--patchonly" ]; then
+    if [ ! -d "/patches" ] || ! ls /patches/*.patch 1>/dev/null 2>&1; then
+        echo "Error: --patchonly specified but no patches found in /patches"
+        exit 1
+    fi
+fi
+
+# If there are patches (even in normal build), copy them and modify spec file
 if [ -d "/patches" ] && ls /patches/*.patch 1>/dev/null 2>&1; then
     echo ">>> Copying patches to SOURCES..."
     cp /patches/*.patch /home/kernelbuilder/rpmbuild/SOURCES/
@@ -55,21 +63,29 @@ sudo dnf builddep -y rpmbuild/SPECS/kernel.spec
 # Unpack the kernel source tarball
 echo ">>> Unpacking kernel source tarball..."
 cd /home/kernelbuilder/rpmbuild/SPECS
+# -bp is Build Prep. We do this even for --patchonly to make sure the patches apply properly
 rpmbuild -bp kernel.spec
 
-# Building
-echo ">>> Building..."
-# --define "_smp_mflags -j8" but rpmbuild will autodetect cpu count
-rpmbuild -bb kernel.spec
+if [ "$1" == "--patchonly" ]; then
+    echo ">>> Repackaging modified SRPM..."
+    rm /home/kernelbuilder/rpmbuild/SRPMS/kernel.src.rpm # delete the original
+    rpmbuild -bs kernel.spec # -bs is Build Source
+    
+    echo ">>> Copying new SRPM to output folder..."
+    cp /home/kernelbuilder/rpmbuild/SRPMS/*.src.rpm /home/kernelbuilder/output/
+    echo ">>> Patch-only build complete. Copied patched SRPM to output folder."
+else
+    echo ">>> Building Binary RPMs..."
+    # --define "_smp_mflags -j8" but rpmbuild will autodetect cpu count
+    rpmbuild -bb kernel.spec # -bb is Build Binary
 
-# Mock build
-# echo ">>> Mock building RPMs..."
-# cd /home/kernelbuilder/
-# mkdir -p rpmbuild/RPMS/aarch64
-# touch rpmbuild/RPMS/aarch64/kernel.rpm
-
-# Copy built RPMs
-echo ">>> Copying RPMs to output folder..."
-cp -r /home/kernelbuilder/rpmbuild/RPMS/* /home/kernelbuilder/output/
-
-echo ">>> Build Complete. Copied RPMs to output folder."
+    # Mock build (to quicky test output copying)
+    # echo ">>> Mock building RPMs..."
+    # cd /home/kernelbuilder/
+    # mkdir -p rpmbuild/RPMS/aarch64
+    # touch rpmbuild/RPMS/aarch64/kernel.rpm
+    
+    echo ">>> Copying RPMs to output folder..."
+    cp -r /home/kernelbuilder/rpmbuild/RPMS/* /home/kernelbuilder/output/
+    echo ">>> Build Complete. Copied RPMs to output folder."
+fi
